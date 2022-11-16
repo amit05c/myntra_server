1a097a39d6cd4d5bf201ee7785740d354153a13d
amig05c <amitghosh0595c@gmail.com>
2022-11-16 21:08:32 +0530

    update

--- routes/cart.js
+++ routes/cart.js
@@ -6,21 +6,39 @@ cartRouter.post("/add/:_id",async(req,res)=>{
     let {userId,size}=(req.body)
     let {_id}=(req.params)
    let data= await DataModel.findOne({_id})
-//    res.send({message: data})
-console.log(data)
-   let newCartData=new CartModel({ 
-    category: data.category,
-    item: data.item,
-    title: data.title,
-    size,
-    image_url: data.image_url,
-    userId,
-    productId: data._id
-   })
-   console.log(newCartData)
-    await newCartData.save()
-res.send("sended")
+
+   try{
+    let newCartData=new CartModel({ 
+        category: data.category,
+        item: data.item,
+        title: data.title,
+        size,
+        price:data.price,
+        image_url: data.image_url,
+        userId,
+        productId: data._id
+       })
+       console.log(newCartData)
+        await newCartData.save()
+    res.status(200).send("sended")
+   }
+   catch(err){
+   res.status(400).send("Please login first")
+   }
    
+   
+})
+
+cartRouter.get("/cartData",async(req,res)=>{
+    try{
+        let {userId}=(req.body)
+        console.log(userId)
+        let data= await CartModel.find({userId})
+        res.status(200).send(data)
+    }
+    catch(err){
+         res.status(400).send("something went wrong")
+    }
 })
 
 
