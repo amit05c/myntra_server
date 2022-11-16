2224656f676563593251ad7c7d1cd4bb05228356
amig05c <amitghosh0595c@gmail.com>
2022-11-16 15:59:11 +0530

    edit schema

--- model/data.model.js
+++ model/data.model.js
@@ -1,6 +1,7 @@
 const mongoose= require("mongoose")
 
 const dataSchema= new mongoose.Schema({
+    _id: {type: String},
     category: {type: String, require:true},
     title: {type: String, require : true},
     item: { type: String, require: true},
